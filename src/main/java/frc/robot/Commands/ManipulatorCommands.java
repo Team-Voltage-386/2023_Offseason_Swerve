20b8b423c0d210612a8fd7f37f34e1b12866dd7a
RyanCofer <rcofer56@gmail.com>
2023-10-08 10:42:01 -0400

    Added Controller Constants

--- src/main/java/frc/robot/Commands/ManipulatorCommands.java
+++ src/main/java/frc/robot/Commands/ManipulatorCommands.java
@@ -9,7 +9,7 @@ import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+import frc.robot.Constants.ControllerConstants;
 
 public class ManipulatorCommands extends CommandBase {
   private Pneumatics PneumaticSystem;
@@ -30,7 +30,7 @@ public class ManipulatorCommands extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (kManipulator.getRawButton(kRightBumper)) 
+    if (ControllerConstants.kManipulator.getRawButton(ControllerConstants.kRightBumper)) 
     {
       
     }
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Constants {
     public static class ID {
@@ -38,5 +39,30 @@ public class Constants {
         public static final double kFrontRightOffset = -1.8959;
         public static final double kBackLeftOffset = -0.8375;
         public static final double kBackRightOffset = -0.3880;
+    }
+
+    /** the indexes to address buttons on the controller */
+    public static final class ControllerConstants {
+        public static final double kDeadband = .1;
+        public static final Joystick kDriver = new Joystick(0);
+        public static final Joystick kManipulator = new Joystick(1);
+
+        public static final int kLeftVertical = 1;
+        public static final int kRightVertical = 5;
+        public static final int kLeftHorizontal = 0;
+        public static final int kRightHorizontal = 4;
+        public static final int kLeftTrigger = 2;
+        public static final int kRightTrigger = 3;
+
+        public static final int kA = 1;
+        public static final int kB = 2;
+        public static final int kX = 3;
+        public static final int kY = 4;
+        public static final int kLeftBumper = 5;
+        public static final int kRightBumper = 6;
+        public static final int kLeftOptions = 7;
+        public static final int kRightOptions = 8;
+        public static final int kLeftJoystickPressed = 9;
+        public static final int kRightJoystickPressed = 10;
     }
 }
